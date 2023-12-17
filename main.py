8baafccb0427616c1ce3c722cb5229a0adc738a9
yoshimetry <42068026+farahroslend@users.noreply.github.com>
2023-12-18 00:59:38 +0800

    Update main.py
    added spacing to print results

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-city=input("What's the name of the city you were born in? ")
-pet=input("What's the name of your first pet? ")
-print("Here's your band name: "+city+pet)
+city=input("What's the name of the city you were born in?\n")
+pet=input("What's the name of your first pet?\n")
+print("\nHere's your band name:\n"+city+pet)
 print("Character length is "+str(len(city+pet)))
