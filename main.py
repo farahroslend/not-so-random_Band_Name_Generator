0e39b5c92a18acaa14e36b455791a864f347ea88
farahroslend <1979080-farahroslend@users.noreply.replit.com>
2023-12-17 16:30:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+city=input("What's the name of the city you were born in? ")
+pet=input("What's the name of your first pet? ")
+print("Here's your band name: "+city+pet)
+print("Character length is "+str(len(city+pet)))
