14cdcf248382b2cade4932413aec5ba482f415d2
yoshimetry <42068026+farahroslend@users.noreply.github.com>
2023-12-18 19:56:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+print('''     
+     ; 
+     ;;
+     ;';.
+     ;  ;;
+     ;   ;;
+     ;    ;;
+     ;    ;;
+     ;   ;'
+     ;  ' 
+,;;;,; 
+;;;;;;
+`;;;;' 
+''')
 city=input("What's the name of the city you were born in?\n")
 pet=input("What's the name of your first pet?\n")
 print("\nHere's your band name:\n"+city+pet)
